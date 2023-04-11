398d07b2a8ba7a776a04868d2bdb2b48d5c8b48b
Marc Farré <contact@marc.fun>
2023-04-11 22:35:41 +0200

    update

--- views/config/index.php
+++ views/config/index.php
@@ -17,10 +17,27 @@ use yii\bootstrap\Alert;
 <div class="container-fluid">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <?= Yii::t('AuthKeycloakModule.base', '<strong>Keycloak</strong> Sign-In configuration') ?></div>
+            <?= Yii::t('AuthKeycloakModule.base', '<strong>Keycloak</strong> Sign-In configuration') ?>
+        </div>
 
         <div class="panel-body">
             <div class="alert alert-info">
+                This module was created and is maintained by
+                <a href="https://www.cuzy.app/"
+                   target="_blank">CUZY.APP (view other modules)</a>.
+                <br>
+                It's free, but it's the result of a lot of design and maintenance work over time.
+                <br>
+                If it's useful to you, please consider
+                <a href="https://www.cuzy.app/checkout/donate/"
+                   target="_blank">making a donation</a>
+                or
+                <a href="https://github.com/cuzy-app/humhub-modules-auth-keycloak"
+                   target="_blank">participating in the code</a>.
+                Thanks!
+            </div>
+
+            <div>
                 <div><?= Yii::t('AuthKeycloakModule.base', 'On Keycloak, create a client for Humhub and configure it:') ?></div>
                 <ul>
                     <li><?= Yii::t('AuthKeycloakModule.base', '{Settings} tab -> {ClientAuthenticationOn} (for Keycloak version <20: {AccessTypeValue}).', [
