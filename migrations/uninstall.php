d5ae9ffae898f471945def5d27e8ed6e54b62a1a
Marc Farré <contact@marc.fun>
2024-05-15 22:45:46 +0200

    update

--- migrations/uninstall.php
+++ migrations/uninstall.php
@@ -1,17 +1,13 @@
 <?php
 
 use humhub\components\Migration;
-use humhub\modules\user\models\Group;
 
 class uninstall extends Migration
 {
 
     public function up()
     {
-        $tableSchema = Yii::$app->getDb()->getSchema()->getTableSchema(Group::tableName(), true);
-        if (in_array('keycloak_id', $tableSchema->columnNames, true)) {
-            $this->safeDropColumn('{{%group}}', 'keycloak_id');
-        }
+        $this->safeDropColumn('{{%group}}', 'keycloak_id');
     }
 
     public function down()
